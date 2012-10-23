4db69aa5e7ad5d5133297eba9808b7493e55fdaa
jrc88 <USER@DOMAIN.COM>
2012-10-23 01:43:57 +0000

    theme update
    git-svn-id: https://svn.library.cornell.edu/dcapsskeleton/trunk@12 8403f5de-c59e-464c-a6ae-807b0a7e3462

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -16,173 +16,101 @@
     </div><!-- cu-identity-wrap -->
   </div>
 </div>
-<div id="wrap">
-    <div class="container">
-        
-        <div class="resize"></div>
-        
-        <!-- #header -->
-        <div id="header" class="sixteen columns clearfix">
-            <div class="inner">
-    
-                <?php if ($logo): ?>
-                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-                  </a>
-                <?php endif; ?>
-                
-                <?php if ($site_name || $site_slogan): ?>
-                <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-                
-                    <?php if ($site_name): ?>
-                    <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-                    </div>
-                    <?php endif; ?>
-                    
-                    <?php if ($site_slogan): ?>
-                    <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-                    <?php print $site_slogan; ?>
-                    </div>
-                    <?php endif; ?>
-                
-                </div>
-                <?php endif; ?>
-            </div>
-        </div><!-- /#header -->
-        
-        <!-- #navigation -->
-        <div id="navigation" class="sixteen columns clearfix">
-        
-            <div class="menu-header">
-            <?php if ($page['header']) : ?>
-                <?php print drupal_render($page['header']); ?>
-                <?php else : ?>
-                <?php print theme('links__system_main_menu', array(
-                'links' => $main_menu,
-                'attributes' => array(
-                    'id' => 'main-menu-links',
-                    'class' => array('menu', 'clearfix'),
-                ),
-                'heading' => array(
-                    'text' => t('Main menu'),
-                    'level' => 'h2',
-                    'class' => array('element-invisible'),
-                ),
-                )); ?>
-            <?php endif; ?>
-            </div>
-            
-        </div><!-- /#navigation -->
-        
-        <?php if ($page['sidebar_first']) { ?>
-        <div id="content" class="eleven columns">
-        <?php } else { ?>
-        <div id="content" class="sixteen columns clearfix">
-        <?php } ?>
-        
-            <?php if ($messages): ?>
-                <div id="messages">
-                  <?php print $messages; ?>
-                </div><!-- /#messages -->
-            <?php endif; ?>
-        
-            <?php if ($breadcrumb): ?>
-                <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-            <?php endif; ?>
-            
-            <div id="main">
-            
-                <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-                
-                <?php print render($title_prefix); ?>
-                
-                <?php if ($title): ?>
-                <h1 class="title" id="page-title">
-                  <?php print $title; ?>
-                </h1>
-                <?php endif; ?>
-                
-                <?php print render($title_suffix); ?>
-                
-                <?php if ($tabs): ?>
-                <div class="tabs">
-                  <?php print render($tabs); ?>
-                </div>
-                <?php endif; ?>
-                
-                <?php print render($page['help']); ?>
-                
-                <?php if ($action_links): ?>
-                <ul class="action-links">
-                  <?php print render($action_links); ?>
-                </ul>
-                <?php endif; ?>
-                
-                <?php print render($page['content']); ?>
-                <?php print $feed_icons; ?>
-                
-            </div>
-        
-        </div><!-- /#content -->
-        
-        <?php if ($page['sidebar_first']): ?>
-        <!-- #sidebar-first -->
-        <div id="sidebar" class="five columns">
-            <?php print render($page['sidebar_first']); ?>
-        </div><!-- /#sidebar-first -->
-        <?php endif; ?>
-        
-        <div class="clear"></div>
-        
-        <?php if ($page['featured_left'] || $page['featured_right']): ?>
-        <!-- #featured -->
-        <div id="featured" class="sixteen columns clearfix">
-            
-            <?php if ($page['featured_left'] && $page['featured_right']) { ?>
-            <div class="one_half">
-            <?php print render($page['featured_left']); ?>
-            </div>
-            
-            <div class="one_half last">
-            <?php print render($page['featured_right']); ?>
-            </div>
-            <?php } else { ?>
-                
-            <?php print render($page['featured_left']); ?>
-            <?php print render($page['featured_right']); ?>
-            
-            <?php } ?>
-            
-        </div><!-- /#featured -->
-        <?php endif; ?>
-        
-        <div class="clear"></div>
-        
-        <div id="footer" class="sixteen columns clearfix">
-        
-            <div class="one_third">
-            <?php if ($page['footer_first']): ?><?php print render($page['footer_first']); ?><?php endif; ?>
-            </div>
-            
-            <div class="one_third">
-            <?php if ($page['footer_second']): ?><?php print render($page['footer_second']); ?><?php endif; ?>
-            </div>
-            
-            <div class="one_third last">
-            <?php if ($page['footer_third']): ?><?php print render($page['footer_third']); ?><?php endif; ?>
-            </div>
-    
-            <div class="clear"></div>
-            
-            <?php if ($page['footer']): print render($page['footer']); endif; ?>
-            
-            <div class="clear"></div>
-            
-            <div id="credits">2012 Your Site - This is a Free Drupal Theme<br/>
-            Ported to Drupal for the Open Source Community by <a href="http://www.drupalizing.com" target="_blank">Drupalizing</a>, a Project of <a href="http://www.morethanthemes.com" target="_blank">More than (just) Themes</a>. Original design by <a href="http://www.simplethemes.com/" target="_blank">Simple Themes</a>.
-            </div>
-        
+<header>
+  <div class="container">
+    <div class="sixteen columns">
+      <div class="row">
+        <div class="ten columns alpha">
+          <h1><a href="<?php print $front_page; ?>"><?php print $site_name; ?></a></h1>
         </div>
+        <div class="six columns omega">
+          <h2><?php print $site_slogan; ?></h2>
+        </div>
+      </div>
+      <nav>
+        <?php print render($page['navigation']); ?>
+      </nav>
     </div>
-</div> <!-- /#wrap -->
+  </div>
+</header>
+
+?>
+
+
+<div class="main-content">
+  <div class="container">
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar  = render($page['sidebar']);
+      ?>
+
+    <!--if there is a sidebar, then create two columned layout-->
+    <?php if ($sidebar): ?>
+    <!--<?php print $breadcrumb; ?>-->
+     <div class="four columns sidebar">
+        <?php print $sidebar; ?>
+      </div>
+      <div class="eleven columns offset-by-one">
+        <?php print render($page['highlighted']); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php if ($title): ?>
+            <h2><?php print $title; ?></h2>
+        <?php endif; ?>
+
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        
+        <?php if(!empty($page['content'])) : ?>
+
+          <?php if(drupal_is_front_page()) {
+              unset($page['content']['system_main']['default_message']);
+            }?>
+
+          <?php print render($page['content']); ?>
+
+        <?php endif; ?>
+      </div>
+
+    <!-- otherwise render a one-column layout -->
+      <?php else :?>
+        <div class="sixteen columns">
+          <?php print render($page['highlighted']); ?>
+          <!--<?php print $breadcrumb; ?>-->
+          <?php print $messages; ?>
+          <?php print render($tabs); ?>
+          <?php if ($title): ?>
+              <h2><?php print $title; ?></h2>
+          <?php endif; ?>
+
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?>
+              <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          
+          <?php if(!empty($page['content'])) : ?>
+
+            <?php if(drupal_is_front_page()) {
+                unset($page['content']['system_main']['default_message']);
+              }?>
+
+            <?php print render($page['content']); ?>
+
+          <?php endif; ?>
+        </div>
+    <?php endif; ?>
+
+  </div>
+</div>
+<footer>
+  <div class="container">
+    <div class="sixteen columns">
+      <?php print render($page['footer']); ?>
+      <?php if ( user_is_logged_in() ) { print '<p class="login">Hi <a href="/user" title="View your dashboard">'. $user->name .'</a> (<a href="/user/logout" title="Logout">Logout</a>)</p>'; } else { print '<p class="login"><a id="login" href="/user" title="Login here">Login</a></p>'; } ?>
+    </div>
+  </div>
+</footer>
+
+<?php print render($page['bottom']); ?>
