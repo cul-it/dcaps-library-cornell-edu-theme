88b7a29f2c5c4bafedc47e530880f2be131014fb
jrc88 <USER@DOMAIN.COM>
2012-10-23 17:21:33 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/dcapsskeleton/trunk@39 8403f5de-c59e-464c-a6ae-807b0a7e3462

--- template.php
+++ template.php
@@ -234,7 +234,7 @@ function dcapsskeleton_breadcrumb($variables) {
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-    $crumbs = '<ul class="breadcrumbs clearfix">';
+    $crumbs .= '<ul class="breadcrumbs clearfix">';
     $array_size = count($breadcrumb);
     $i = 0;
     while ( $i < $array_size) {
