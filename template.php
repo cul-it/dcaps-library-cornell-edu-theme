f2218aaac23b02f38928eaadf8da06b17287d883
jrc88 <USER@DOMAIN.COM>
2012-10-23 17:32:03 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/dcapsskeleton/trunk@44 8403f5de-c59e-464c-a6ae-807b0a7e3462

--- template.php
+++ template.php
@@ -226,17 +226,31 @@ function dcapsskeleton_preprocess_block(&$variables, $hook) {
 // */
 
 /* Put Breadcrumbs in a ul li structure */
+/**
+* Output breadcrumb as an unorderd list with unique and first/last classes
+*/
 function dcapsskeleton_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
-
   if (!empty($breadcrumb)) {
-      $crumbs = '<ul class="breadcrumbs">';
-
-      foreach($breadcrumb as $value) {
-           $crumbs .= '<li>'.$value.'</li>';
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $crumbs = '<ul class="breadcrumbs clearfix">';
+    $array_size = count($breadcrumb);
+    $i = 0;
+    while ( $i < $array_size) {
+      $crumbs .= '<li class="breadcrumb-' . $i;
+      if ($i == 0) {
+        $crumbs .= ' first';
       }
-      $crumbs .= '</ul>';
+      if ($i+1 == $array_size) {
+        $crumbs .= ' last';
+      }
+      $crumbs .=  '">' . $breadcrumb[$i] . '</li>';
+      $i++;
     }
-      return $crumbs;
+    $crumbs .= '</ul>';
+    return $crumbs;
   }
+}
 ?>
