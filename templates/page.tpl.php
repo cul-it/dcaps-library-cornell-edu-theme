638d31adc2d2b7f6614c1342c61bd9c7dcd7b0fe
jrc88 <USER@DOMAIN.COM>
2012-10-24 17:38:17 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/dcapsskeleton/trunk@76 8403f5de-c59e-464c-a6ae-807b0a7e3462

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -67,10 +67,11 @@
     print $title;
   }
 ?>
-
+</div>
       <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', $menuParent['title'])), 'heading' => NULL)); ?>
         <?php print $sidebar; ?>
       </div>
+
 
 
       <div class="eleven columns offset-by-one">
