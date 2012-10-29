b510cd8895691247cb783dbb3940290f87b9afb7
jrc88 <USER@DOMAIN.COM>
2012-10-29 14:45:49 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/dcapsskeleton/trunk@89 8403f5de-c59e-464c-a6ae-807b0a7e3462

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -54,17 +54,8 @@
      <div class="four columns sidebar">
       <div class="nav-header">
       <?php
-  $menuParent = menu_get_active_trail();
-  //get rid of the last item in the array as it is the current page
-  $menuParentPop = array_pop($menuParent);
-  //Just grab the last item in the array now
-  $menuParent = end($menuParent);
-  $menu_parent_title = $menuParent['title'];
-  //if it is not the home page and it is not an empty array
-  if(!empty($menuParent) && $menuParent['link_path'] != ''){
-    print $menuParent['title'];
-  } else{
-    print $title;
+  
+    print $section_title;
   }
 ?>
 </div>
