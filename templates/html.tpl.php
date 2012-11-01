dd9c527296d8df46ff7198b4dab6012f3c7e4a1a
jrc88 <USER@DOMAIN.COM>
2012-11-01 17:44:40 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/dcapsskeleton/trunk@134 8403f5de-c59e-464c-a6ae-807b0a7e3462

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -86,7 +86,12 @@
   <!--[if lt IE 9]>
   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
-  <script type="text/javascript">
+ 
+  <link rel="shortcut icon" href="../images/favicon.ico">
+  <link rel="apple-touch-icon" href="../images/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="../images/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="../images/apple-touch-icon-114x114.png">
+ <script type="text/javascript">
       $(document).ready(function() {
       $('.show_hide').showHide({       
       speed: 500,
@@ -94,11 +99,6 @@
         });
     })
    </script>
-  <link rel="shortcut icon" href="../images/favicon.ico">
-  <link rel="apple-touch-icon" href="../images/apple-touch-icon.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="../images/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="../images/apple-touch-icon-114x114.png">
-
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php print $page_top; ?>
