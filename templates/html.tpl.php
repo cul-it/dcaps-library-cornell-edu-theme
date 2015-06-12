5f817e2733271782f6f8ff7cb5f7b006d6df8ede
Melissa Wallace <mhk33@cornell.edu>
2015-06-12 15:00:26 -0400

    initial commit of clean bootstrap theme

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,19 +1,15 @@
 <?php
 /**
  * @file
- * Zen theme's implementation to display the basic html structure of a single
+ * Default theme implementation to display the basic html structure of a single
  * Drupal page.
  *
  * Variables:
  * - $css: An array of CSS files for the current page.
  * - $language: (object) The language the site is being displayed in.
- *   $language->language contains its textual representation. $language->dir
- *   contains the language direction. It will either be 'ltr' or 'rtl'.
- * - $html_attributes: String of attributes for the html element. It can be
- *   manipulated through the variable $html_attributes_array from preprocess
- *   functions.
- * - $html_attributes_array: Array of html attribute values. It is flattened
- *   into a string within the variable $html_attributes.
+ *   $language->language contains its textual representation.
+ *   $language->dir contains the language direction. It will either be 'ltr' or
+ *   'rtl'.
  * - $rdf_namespaces: All the RDF namespace prefixes used in the HTML document.
  * - $grddl_profile: A GRDDL profile allowing agents to extract the RDF data.
  * - $head_title: A modified version of the page title, for use in the TITLE
@@ -27,15 +23,9 @@
  *   - slogan: The slogan of the site, if any, and if there is no title.
  * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
  *   so on).
- * - $default_mobile_metatags: TRUE if default mobile metatags for responsive
- *   design should be displayed.
  * - $styles: Style tags necessary to import all CSS files for the page.
  * - $scripts: Script tags necessary to load the JavaScript files and settings
  *   for the page.
- * - $skip_link_anchor: The HTML ID of the element that the "skip link" should
- *   link to. Defaults to "main-menu".
- * - $skip_link_text: The text for the "skip link". Defaults to "Jump to
- *   Navigation".
  * - $page_top: Initial markup from any modules that have altered the
  *   page. This variable should always be output first, before all other dynamic
  *   content.
@@ -43,56 +33,44 @@
  * - $page_bottom: Final closing markup from any modules that have altered the
  *   page. This variable should always be output last, after all other dynamic
  *   content.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It should be placed within the <body> tag. When selecting through CSS
- *   it's recommended that you use the body tag, e.g., "body.front". It can be
- *   manipulated through the variable $classes_array from preprocess functions.
- *   The default values can contain one or more of the following:
- *   - front: Page is the home page.
- *   - not-front: Page is not the home page.
- *   - logged-in: The current viewer is logged in.
- *   - not-logged-in: The current viewer is not logged in.
- *   - node-type-[node type]: When viewing a single node, the type of that node.
- *     For example, if the node is a Blog entry, this would be "node-type-blog".
- *     Note that the machine name of the content type will often be in a short
- *     form of the human readable label.
- *   The following only apply with the default sidebar_first and sidebar_second
- *   block regions:
- *     - two-sidebars: When both sidebars have content.
- *     - no-sidebars: When no sidebar content exists.
- *     - one-sidebar and sidebar-first or sidebar-second: A combination of the
- *       two classes when only one of the two sidebars have content.
+ * - $classes String of classes that can be used to style contextually through
+ *   CSS.
  *
+ * @see bootstrap_preprocess_html()
  * @see template_preprocess()
  * @see template_preprocess_html()
- * @see zen_preprocess_html()
  * @see template_process()
+ *
+ * @ingroup themeable
  */
-?>
-<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
-<head>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
+<head profile="<?php print $grddl_profile; ?>">
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php print $head; ?>
   <title><?php print $head_title; ?></title>
-  <meta name="description" content="Digital Consulting and Production Services">
-  <meta name="author" content="Cornell University Library Digital Scholarship and Preservation Services">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <?php print $styles; ?>
-  <?php print $scripts; ?>
+  <link href="/sites/all/themes/olinuris/css/style.css" rel="stylesheet">
+  <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
-  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
- 
-  <link rel="shortcut icon" href="../images/favicon.png">
-
+  <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <script>
+    jQuery(document).ready(function() { 
+      jQuery( '.cul-icon-chevron' ).before( '<i class="fa fa-chevron-right"></i>' );
+    });
+  </script>
 </body>
 </html>
