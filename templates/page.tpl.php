e948ab2aa480367010d9b23409a853e39a8cd78f
Melissa Wallace <melissakuo@gmail.com>
2015-06-14 19:30:28 -0400

    fix cornell bar

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -76,7 +76,7 @@
 
 <div class="cu-identity">
   <div class="container">
-    <div id="cu-identity-wrap">
+    <div id="cu-identity-wrap" class="col-sm-8">
       <div id="cu-identity-content"> <a id="insignia-link" href="http://www.cornell.edu/"><img src="/sites/all/themes/dcaps/img/lib.gif" alt="Cornell University" border="0" /></a>
         <div id="unit-signature-links"> <a id="cornell-link" href="http://www.library.cornell.edu/">Cornell University Library</a> </div>
         <div id="search-navigation" class="mobile-hide">
